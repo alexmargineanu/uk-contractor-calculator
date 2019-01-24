04cd82e50053999cb5b09e67922198b2316332cd
Alecu <alex.margineanu@gmail.com>
2019-01-24 08:16:27 +0000

    Updates

---
+++ precache-manifest.20c7ab2a8a2b262e69e6980732a38eac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fa0f59ccfa28ccfe052",
+    "url": "/uk-contractor-calculator/static/js/runtime~main.9fa0f59c.js"
+  },
+  {
+    "revision": "524b1631fc0dd74c856d",
+    "url": "/uk-contractor-calculator/static/js/main.524b1631.chunk.js"
+  },
+  {
+    "revision": "a99787bbf25f94db6d3d",
+    "url": "/uk-contractor-calculator/static/js/1.a99787bb.chunk.js"
+  },
+  {
+    "revision": "524b1631fc0dd74c856d",
+    "url": "/uk-contractor-calculator/static/css/main.f1fd73c4.chunk.css"
+  },
+  {
+    "revision": "b4f52a27be38981d4bfb7fdc8f935906",
+    "url": "/uk-contractor-calculator/index.html"
+  }
+];

2d0944cf072e8093809c8da8d05fcebc3ed8be81
Alecu <alex.margineanu@gmail.com>
2019-01-24 08:41:25 +0000

    net pay without progress

--- src/NetPay/index.js
+++ src/NetPay/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Segment, Progress } from 'semantic-ui-react'
+import { Segment, Statistic } from 'semantic-ui-react'
 import PropTypes from 'prop-types';
 import * as util from '../util.js';
 
@@ -8,18 +8,23 @@ const NetPay = ({ weeklyRate, nationalInsurance, incomeTax, noWeeks }) => {
   const net = gross - (nationalInsurance * noWeeks) - incomeTax;
 
   return (
-    <Segment>
-      {incomeTax > 0 && (
-        <Progress color='pink' percent={Math.floor(incomeTax * (100 / gross))} progress>
-          {util.financial(incomeTax)} of your gross pay goes to Income Tax
-        </Progress>
-      )}
-      <Progress color='violet' percent={Math.floor(nationalInsurance * noWeeks * (100 / gross))} progress>
-        {util.financial(nationalInsurance * noWeeks)} of your gross pay goes to National Insurance
-      </Progress>
-      <Progress color='green' percent={Math.ceil(net * (100 / gross))} progress>
-        {util.financial(net)} NET pay
-      </Progress>
+    <Segment inverted className='netPay'>
+      <Statistic size='small' color='teal'>
+        <Statistic.Value>{util.financial(gross)}</Statistic.Value>
+        <Statistic.Label>GROSS Pay</Statistic.Label>
+      </Statistic>
+      <Statistic size='small' color='green'>
+        <Statistic.Value>{util.financial(net)}</Statistic.Value>
+        <Statistic.Label>NET Pay</Statistic.Label>
+      </Statistic>
+      <Statistic size='small' color='pink'>
+        <Statistic.Value>{util.financial(incomeTax)}</Statistic.Value>
+        <Statistic.Label>Income Tax</Statistic.Label>
+      </Statistic>
+      <Statistic size='small' color='orange'>
+        <Statistic.Value>{util.financial(nationalInsurance * noWeeks)}</Statistic.Value>
+        <Statistic.Label>National Insurance</Statistic.Label>
+      </Statistic>
     </Segment>
   );
 }
